fcccdce095ff675d7da5c3ae444932a0a1bccdd1
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-06-04 14:17:51 +0200

    added qr code generator and and made static ip addresses static

---
+++ src/main/java/com/example/loadingbackend/QRcodeGenerator.java
@@ -0,0 +1,21 @@
+package com.example.loadingbackend;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+
+import java.awt.image.BufferedImage;
+
+public class QRcodeGenerator {
+
+    public static BufferedImage generateQRCode(String text, int width, int height) throws WriterException {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+}
---
+++ src/main/java/com/example/loadingbackend/Services/QRService.java
@@ -0,0 +1,46 @@
+package com.example.loadingbackend.Services;
+
+import com.example.loadingbackend.QRcodeGenerator;
+import com.google.zxing.WriterException;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+
+@Service
+public class QRService {
+
+    public byte[] generateVoteQRCode() {
+
+        String text = null;
+        try {
+            text = getIpAddress();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int width = 300;
+        int height = 300;
+        BufferedImage qrCode = null;
+        try {
+            qrCode = QRcodeGenerator.generateQRCode(text, width, height);
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            ImageIO.write(qrCode, "png", byteStream);
+            return byteStream.toByteArray();
+        } catch (WriterException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public String getIpAddress() throws IOException {
+    InetAddress ip = InetAddress.getLocalHost();
+    String ipAddress = ip.getHostAddress();
+    ipAddress = "http://" + ipAddress + ":8080" + "/vote";
+    return ipAddress;
+    }
+}
