bf1a0f919dd6a8f48a65c36ef1318abde008fdb6
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-06-04 10:23:08 +0200

    added json resp to vote controller

--- src/main/java/com/example/loadingbackend/Controllers/VoteController.java
+++ src/main/java/com/example/loadingbackend/Controllers/VoteController.java
@@ -3,7 +3,10 @@ package com.example.loadingbackend.Controllers;
 import com.example.loadingbackend.Models.VoteModel;
 import com.example.loadingbackend.Services.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.google.gson.Gson;
+
 
 @RequestMapping("api/vote")
 @RestController
@@ -12,6 +15,7 @@ public class VoteController {
 
     //Declares the service
     private final VoteService voteService;
+    private static final Gson gson = new Gson();
 
     //Autowire links the service to the controller
     @Autowired
@@ -23,7 +27,9 @@ public class VoteController {
     //Post mapping which adds a vote to the vote service
     //Note that the return value of the function is what the server will return to the client
     @PostMapping
-    public void addVote(@RequestBody VoteModel voteModel){
+    public ResponseEntity<String> addVote(@RequestBody VoteModel voteModel){
         voteService.addVote(voteModel);
+
+        return ResponseEntity.ok(gson.toJson("Vote added"));
     }
 }
