4b72ee7c82cd326238de79b0418a74a2d609937e
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-06-04 09:54:14 +0200

    added cross origin access

--- src/main/java/com/example/loadingbackend/Controllers/VoteController.java
+++ src/main/java/com/example/loadingbackend/Controllers/VoteController.java
@@ -3,13 +3,11 @@ package com.example.loadingbackend.Controllers;
 import com.example.loadingbackend.Models.VoteModel;
 import com.example.loadingbackend.Services.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/vote")
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class VoteController {
 
     //Declares the service
