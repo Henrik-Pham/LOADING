5459b9c98336395b965d48bbf4b638694d0b3587
chris <c.loyd87@gmail.com>
2024-06-05 13:41:40 +0200

    Some content in info-box on MainPage. ItemInfo.jsx component in separate folder for components, array of objects in infoBoxData.js. Some tweaks on css, but not finished. Also added images in separate folder (images)

---
+++ src/main/resources/templates/infoBoxData.js
@@ -0,0 +1,17 @@
+import swordImg from './images/Sword_Cave.webp'
+import fuImg from './images/FU_FPV.webp';
+
+export const INFO_BOX_CONTENT = [
+    {
+        image: swordImg,
+        title: "Sword of the Cave",
+        //stats: "Damage +2.",
+        description: "A sword made from the rarest materials found in the deepest caves."
+    },
+    {
+        image: fuImg,
+        title: "The Finger of the Universe",
+        //stats: "Enraged enemy +10.",
+        description: "A finger summon the deepest of rage in your enemies, making them highly agressive."
+    }
+];
