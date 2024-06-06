0b10161df27ae6c66773293cf5a64a3e140d330e
Henrik-Pham <HenrikPHPham@gmail.com>
2024-06-06 14:09:17 +0200

    lagt til front end kode fra Christopher

---
+++ src/main/resources/templates/ItemInfo.jsx
@@ -0,0 +1,11 @@
+
+function ItemInfo({ image, alt, title, description }) {
+    return (
+        <div className="item-info-container">
+            <img className="item-info-img" src={image} alt={alt} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    );
+}
+export default ItemInfo;
